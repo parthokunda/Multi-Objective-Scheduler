1eef11fb47687f016e95ff0f4dfd1e2f94e63502
root <root@k8s-vm4.novalocal>
2023-12-19 17:11:11 +0000

    CPU usage added, naming fixed for RPS

--- loadTest/plotAllCpu.py
+++ loadTest/plotAllCpu.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-nMCPU = pd.read_csv('netMarks_load_2500_5m_cpuUsage.csv')
-v1CPU = pd.read_csv('v1_load_2500_5m_cpuUsage.csv')
+dfs = []
+dfsByNodes : list[dict] = {}
+nodes = ['k8s-vm1','k8s-vm2','k8s-vm3','k8s-vm5']
+files = ['1-cpuUsage.csv','2-cpuUsage.csv', '3-cpuUsage.csv', '4-cpuUsage.csv',
+         '5-cpuUsage.csv']
+labels = ['default','.25', '.5', '.75', '1']
 
-nM_start = nMCPU['timestamp'].min()
-v1_start = v1CPU['timestamp'].min()
+for file in files:
+    dfs.append(pd.read_csv("./data/"+file))
 
-nMCPU['timestamp'] = nMCPU['timestamp'] - nM_start
-v1CPU['timestamp'] = v1CPU['timestamp'] - v1_start
+for i, df in enumerate(dfs):
+    mx = dfs[i]['timestamp'].max()
+    dfs[i].loc[:, ('timestamp')] = dfs[i][dfs[i]['timestamp'] > mx - 600]
+    mn = dfs[i]['timestamp'].min()
+    dfs[i].loc[:, ('timestamp')] = dfs[i].loc[:, ('timestamp')] - mn
+    # nodeCPU = {}
+    # for node in nodes:
+    #     nodeCPU[node] = dfs[i][dfs[i]['node_name'] == 'k8s-vm1']
+    # dfsByNodes.append(nodeCPU)
 
-nMvm1 = nMCPU[nMCPU['node_name'] == 'k8s-vm1']
-nMvm2 = nMCPU[nMCPU['node_name'] == 'k8s-vm2']
-nMvm3 = nMCPU[nMCPU['node_name'] == 'k8s-vm3']
+# nMCPU = pd.read_csv('netMarks_load_2500_5m_cpuUsage.csv')
+# v1CPU = pd.read_csv('v1_load_2500_5m_cpuUsage.csv')
 
-v1vm1 = v1CPU[nMCPU['node_name'] == 'k8s-vm1']
-v1vm2 = v1CPU[nMCPU['node_name'] == 'k8s-vm2']
-v1vm3 = v1CPU[nMCPU['node_name'] == 'k8s-vm3']
+# nM_start = nMCPU['timestamp'].min()
+# v1_start = v1CPU['timestamp'].min()
+
+# nMCPU['timestamp'] = nMCPU['timestamp'] - nM_start
+# v1CPU['timestamp'] = v1CPU['timestamp'] - v1_start
+
+# nMvm1 = nMCPU[nMCPU['node_name'] == 'k8s-vm1']
+# nMvm2 = nMCPU[nMCPU['node_name'] == 'k8s-vm2']
+# nMvm3 = nMCPU[nMCPU['node_name'] == 'k8s-vm3']
+
+# v1vm1 = v1CPU[nMCPU['node_name'] == 'k8s-vm1']
+# v1vm2 = v1CPU[nMCPU['node_name'] == 'k8s-vm2']
+# v1vm3 = v1CPU[nMCPU['node_name'] == 'k8s-vm3']
 
 
 # datalowdefault = datalowdefault[datalowdefault['Requests/s'] > 0]
@@ -24,33 +44,18 @@ v1vm3 = v1CPU[nMCPU['node_name'] == 'k8s-vm3']
 # # datalowdefault['Readable_Time'] = pd.to_datetime(datalowdefault['Timestamp'], unit='s')
 # # print(datalowdefault['Readable_Time'])
 
-fig, axs = plt.subplots(3,1,figsize=(12,8))
+fig, axs = plt.subplots(5,1,figsize=(24,16))
 
-axs[0].plot(nMvm1['timestamp'],nMvm1['value'], label = 'NetMarks')
-axs[0].plot(v1vm1['timestamp'],v1vm1['value'], color='red', label = 'v1')
-axs[0].grid(True)
-axs[0].set_xlabel('Timestamp')
-axs[0].set_ylabel('k8s-vm1')
-axs[0].legend()
+def plotNodeUsage(df, axs, nodesList, axis, label ,x = 'timestamp', y='value'):
+    for node in nodesList:
+        axs[axis].plot( df[df['node_name'] == node][x], df[df['node_name'] == node][y], label = node)
+        axs[axis].grid(True)
+        axs[axis].set_ylabel(label)
+        axs[axis].legend()
 
-axs[1].plot(nMvm2['timestamp'],nMvm2['value'], label = 'NetMarks')
-axs[1].plot(v1vm2['timestamp'],v1vm2['value'], color='red', label = 'v1')
-axs[1].grid(True)
-axs[1].set_xlabel('Timestamp')
-axs[1].set_ylabel('k8s-vm2')
-axs[1].legend()
+for i in range(len(dfs)):
+    plotNodeUsage(dfs[i], axs, nodes, i, labels[i])
 
 
-axs[2].plot(nMvm3['timestamp'], nMvm3['value'], linestyle='-', label = 'NetMarks')
-axs[2].plot(v1vm3['timestamp'], v1vm3['value'], color='red', label = 'v1')
-axs[2].grid(True)
-axs[2].set_xlabel('Timestamp')
-axs[2].set_ylabel('k8s-vm3')
-axs[2].legend()
-
-
-
-
-# # plt.show()
-fig.suptitle("v1 vs NetMarks CPU usage - 2500user 100spawn 5minute")
-plt.savefig('v1 vs NetMarks CPU usage - 2500user 100spawn 5minute.png')
+fig.suptitle("CPU Stats - 2500user 100spawn 5minute - .25 means 25% network usage")
+plt.savefig('images/CPU stats - Incorporated CPU usage.png')
--- loadTest/plotRPS.py
+++ loadTest/plotRPS.py
@@ -7,40 +7,12 @@ files = ['1-v1_load_2500_8m_stats_history.csv','2-v1_load_2500_8m_stats_history.
 labels = ['default','.25', '.5', '.75', '1']
 
 for file in files:
-    dfs.append(pd.read_csv(file))
+    dfs.append(pd.read_csv('data/'+file))
 
 for i, df in enumerate(dfs):
     dfs[i] = df[df['Name'] == 'Aggregated']
     mn = dfs[i]['Timestamp'].min()
     dfs[i].loc[:, ('Timestamp')] = dfs[i].loc[:, ('Timestamp')] - mn
-
-
-
-# default = pd.read_csv('1-default_load_2500_8m_stats_history.csv')
-# two = pd.read_csv('2-v1_load_2500_8m_stats_history.csv')
-# three = pd.read_csv('3-v1_load_2500_8m_stats_history.csv')
-# four = pd.read_csv('4-v1_load_2500_8m_stats_history.csv')
-
-# default = default[default['Name'] == 'Aggregated']
-# two = two[two['Name'] == 'Aggregated']
-# three = three[three['Name'] == 'Aggregated']
-# four = four[four['Name'] == 'Aggregated']
-
-# nM_start = nM['Timestamp'].min()
-# v1_start = v1['Timestamp'].min()
-
-# nM['Timestamp'] = nM['Timestamp'] - nM_start
-# v1['Timestamp'] = v1['Timestamp'] - v1_start
-
-# default['Timestamp'] = default['Timestamp']  - default['Timestamp'].min()
-# two['Timestamp'] = two['Timestamp']  - two['Timestamp'].min()
-# three['Timestamp'] = three['Timestamp']  - three['Timestamp'].min()
-# four['Timestamp'] = four['Timestamp']  - four['Timestamp'].min()
-
-# default = default[default['Timestamp'] >= 100]
-# two = two[two['Timestamp'] >= 100]
-# three = three[three['Timestamp'] >= 100]
-# four = four[four['Timestamp'] >= 100]
 
 fig, axs = plt.subplots(2,2,figsize=(12,8))
 
@@ -59,12 +31,10 @@ def plotBar(axs, categories, values, axis:tuple, y):
 failure = [df['Total Failure Count'].max() for df in dfs]
 request = [df['Total Request Count'].max() for df in dfs]
 plot(axs, dfs, labels, axis=(0,0), y = 'Requests/s')
-# plot(axs, dfs, labels, axis=(0,1), y = '99%')
 plotBar(axs, labels, failure, axis=(0,1), y='Failure Count')
 plot(axs, dfs, labels, axis=(1,0), y = 'Total Median Response Time')
-# plot(axs, dfs, labels, axis=(1,1), y = 'Total Request Count')
 plotBar(axs, labels, request, axis=(1,1), y='Request Count')
 
 
-fig.suptitle("CPU status - 2500user 100spawn 5minute - .25 means 25% network weight")
-plt.savefig('Incorporating CPU Stats.png')
+fig.suptitle("Net Stats - 2500user 100spawn 5minute - .25 means 25% network weight")
+plt.savefig('images/Net Stats - Incorporating CPU Stats.png')
