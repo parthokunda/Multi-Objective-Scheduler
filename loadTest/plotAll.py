bb21bf8ebab032eab98c4f78d569bac73a5b6e26
root <76593389+piratepartho@users.noreply.github.com>
2024-01-22 12:32:41 +0000

    hpdc: benchmarks baseline written, plotRequest added

--- loadTest/plotAll.py
+++ loadTest/plotAll.py
@@ -4,15 +4,18 @@ import numpy as np
 
 dfs = []
 slas = []
-files = [87, 88, 89, 90, 91, 92, 93, 94, 95 ]
-labels = [ '0', '.125', '.25', '.375', '.5', '.625', '.75', '.875', '1.0']
+files = [200, 201, 202, 203, 204, 205]
+labels = [50, 100, 500, 1000, 1500, 2000]
+for i, _ in enumerate(labels):
+    labels[i] = str(labels[i])
+
 costs = []
 
 for file in files:
     print(file)
-    dfs.append(pd.read_csv('data/'+str(file)+'_stats_history.csv'))
-    slas.append(pd.read_csv('data/'+str(file)+'-SLA.csv'))
-    costs.append(pd.read_csv('data/'+str(file)+'-costMonitor.csv'))
+    dfs.append(pd.read_csv(str(file)+'_stats_history.csv'))
+    slas.append(pd.read_csv(str(file)+'-SLA.csv'))
+    costs.append(pd.read_csv(str(file)+'-costMonitor.csv'))
 
 for i, df in enumerate(dfs):
     dfs[i] = df[df['Name'] == 'Aggregated']
@@ -20,16 +23,12 @@ for i, df in enumerate(dfs):
     dfs[i].loc[:, ('Timestamp')] = dfs[i].loc[:, ('Timestamp')] - mn
     dfs[i] = dfs[i][dfs[i]['Timestamp'] > 15]
 
-
-fig, axs = plt.subplots(2,2,figsize=(16,12))
-
 def plotCost(axs, labels, axis:tuple, x = 'Net Weight', y='Avg Cost'):
     for i, costDF in enumerate(costs):
         costs[i] = costs[i]['avgCost'].iloc[-1]
     axs[axis[0], axis[1]].bar(labels, costs)
     axs[axis[0], axis[1]].set_ylabel("Average Cost")
     axs[axis[0], axis[1]].set_title("Avg Cost Per Minute")
-
 
 def plot(axs, dfs, labels, axis:tuple, x = 'Timestamp', y='Requests/s'):
     for i in range(len(dfs)):
@@ -58,33 +57,42 @@ def plotSLA(axs, slas, labels, axis:tuple, x = 'Timestamp', y='Requests/s', slaL
     axs[axis[0], axis[1]].legend()
 
     
-def plotSLA2(axs, slas, labels, axis:tuple, x = 'Timestamp', y='Requests/s'):
-    slaLimits = np.linspace(50, 1500, 10)
+def plotSLA2(slas, labels, x = 'Timestamp', y='Requests/s'):
+    slaLimits = np.linspace(0, 5000, 50)
     for slaIndex, sla in enumerate(slas):
         slaViolationPerLimit = []
         for limitIndex, limit in enumerate(slaLimits):
             slaViolationPerLimit.append((sla['response_time'] > limit).sum() / len(sla) * 100)
-        axs[axis[0], axis[1]].plot(slaLimits, slaViolationPerLimit, label = labels[slaIndex])
-    axs[axis[0], axis[1]].set_xlabel("ms")
-    axs[axis[0], axis[1]].set_ylabel("Violation Percentage")
-    axs[axis[0], axis[1]].set_title("SLA Violation Graph")
-    axs[axis[0], axis[1]].legend()
+        ax1.plot(slaLimits, slaViolationPerLimit, label = labels[slaIndex])
+    ax1.set_xlabel("ms", fontsize="xx-large")
+    ax1.set_ylabel("Violation Percentage", fontsize='xx-large')
+    ax1.set_title("SLA Violation Graph", fontsize='xx-large')
+    ax1.tick_params(axis='x', labelrotation=45, labelsize='xx-large')
+    ax1.tick_params(axis='y', labelsize='xx-large')
+    ax1.legend(fontsize='xx-large')
 
-def plotBar(axs, categories, values, axis:tuple, y):
-    axs[axis[0], axis[1]].bar(categories, values)
-    axs[axis[0], axis[1]].set_ylabel(y)
-    axs[axis[0], axis[1]].set_title("Total Requests For Weights")
+# def plotBar(categories, values, y):
+#     ax2.bar(categories, values)
+#     print(categories, values)
+#     ax2.set_ylabel(y, fontsize='xx-large')
+#     ax2.set_title("Total Requests For Weights", fontsize='xx-large')
+#     ax2.tick_params(axis='x', labelrotation=45, labelsize='xx-large')
+#     ax2.tick_params(axis='y', labelsize='xx-large')
 
 
 failure = [df['Total Failure Count'].max() for df in dfs]
 request = [df['Total Request Count'].max() for df in dfs]
 slaLimit = 2000
-plotSLA2(axs, slas, labels, axis=(0,0), y=f'SLA ({slaLimit}ms) Violated %')
+fig, (ax1, ax2) = plt.subplots(1,2,figsize=(24,12))
+print(labels)
+print(request)
+ax2.bar(labels, request)
 # plotBar(axs, labels, failure, axis=(0,1), y='Failure Count')
-plotCost(axs, labels, axis=(0,1))
-plot(axs, dfs, labels, axis=(1,0), y = 'Total Median Response Time')
-plotBar(axs, labels, request, axis=(1,1), y='Request Count')
+# plotCost(axs, labels, axis=(0,1))
+# plot(axs, dfs, labels, axis=(1,0), y = 'Total Median Response Time')
+# plotBar( labels, request, y='Request Count')
+plotSLA2( slas, labels, y=f'SLA ({slaLimit}ms) Violated %')
 
 
-fig.suptitle("Cost = 0.375, Net Weight = .25(relative) CPU Weight .75(relative)\nUser Count = 2000")
-plt.savefig('images/x_Stats')
+# fig.tight_layout(pad=4)
+plt.savefig('images/x_Net_Stats')
---
+++ loadTest/thesis_plots/plotRequests.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import os
+from collections import defaultdict
+os.chdir('/root/loadTest/')
+
+class EntryLoader :
+    def __init__(self):
+        file = open('entries.txt', 'r')
+        lines = file.readlines()
+        self.datas = defaultdict(list)
+        for line in lines:
+            line = eval(line)
+            cost = line['cost_weight']
+            net = line['net_weight']
+            cpu = line['cpu_weight']
+            # make net and cpu weights relative to each other
+            if cost != 1:
+                net = net / (1-cost)
+                cpu = cpu / (1-cost)
+            weights = (net,cpu,cost, line['userCount'])
+            self.datas[weights].append(line['fileNum'])
+
+    # filters fileNumbers from datas, gives a single value even if there are multiples
+    def filter(self, net_weight:list = None, cpu_weight:list = None, cost_weight:list = None, userCount:list = None):
+        filteredDatas = defaultdict(list)
+        for key, values in self.datas.items():
+            if ((net_weight is None or key[0] in net_weight) and
+                (cpu_weight is None or key[1] in cpu_weight) and
+                (cost_weight is None or key[2] in cost_weight) and
+                (userCount is None or key[3] in userCount)):
+                    filteredDatas[key] = values
+        return filteredDatas
+    
+    def getTotalRequests(self, datas: defaultdict(list)):
+        requestDict = defaultdict(int)
+        for key,values in datas.items():
+            request = 0
+            for value in values:
+                df = pd.read_csv('data/hpdc/'+str(value)+'_stats_history.csv')
+                df = df[df['Name'] == 'Aggregated']
+                mn = df['Timestamp'].min()
+                df.loc[:, ('Timestamp')] = df.loc[:, ('Timestamp')] - mn
+                df = df[df['Timestamp'] > 20]
+                request += df['Total Request Count'].max()
+            request /= len(values) # take avg of all available data, if we run multiple benchmarks
+            requestDict[key] = request
+        return requestDict
+
+class BaseLineLoader :
+    def __init__(self):
+        file = open('entries_baselines.txt', 'r')
+        lines = file.readlines()
+        self.datas = defaultdict(list)
+        for line in lines:
+            line = eval(line)
+            self.datas[line['baseline'], line['user']].append(line['fileNum'])
+    
+    def filter(self, baseline:list = None, user:list = None):
+        filterData = defaultdict(list)
+        for key, values in self.datas.items():
+            if ((baseline is None or key[0] in baseline) and
+               (user is None or key[1] in user)):
+                filterData[key] = values
+        return filterData
+    
+    def getTotalRequests(self, datas: defaultdict(list)):
+        requestDict = defaultdict(int)
+        for key, values in datas.items():
+            request = 0
+            for value in values:
+                df = pd.read_csv('data/baselines/'+str(value)+'_stats_history.csv')
+                df = df[df['Name'] == 'Aggregated']
+                mn = df['Timestamp'].min()
+                df.loc[:, ('Timestamp')] = df.loc[:, ('Timestamp')] - mn
+                df = df[df['Timestamp'] > 20]
+                request += df['Total Request Count'].max()
+            request /= len(values) # take avg of all available data, if we run multiple benchmarks
+            requestDict[key] = request
+        return requestDict
+
+entries = EntryLoader()
+filteredDatas = entries.filter(userCount=[1000], cost_weight=[0,.25,.5])
+requests = entries.getTotalRequests(filteredDatas)
+print(requests)
+
+cost_weights = sorted(set(key[2] for key in requests.keys()))
+net_weights = sorted(set(key[0] for key in requests.keys()))
+print(net_weights)
+
+palette = sns.color_palette("tab10")
+fig, ax = plt.subplots()
+bar_width = .2  # Width of individual bars
+group_width = bar_width * len(cost_weights)  # The total width of a group
+gap_width = 0.3 # Width of the gap between groups
+index = np.arange(len(net_weights)) * (group_width + gap_width) 
+
+baselines = 2
+baselineLoader = BaseLineLoader()
+# ax.bar(bar_width+gap_width, 60000, bar_width, label='netMarks', color=palette[-2])
+
+filteredBaselines = baselineLoader.filter(baseline=['default', 'netmarks'],user=[1000])
+baselinesRequest = baselineLoader.getTotalRequests(filteredBaselines)
+print('baselineReq\n', baselinesRequest)
+ax.bar(0, baselinesRequest[('default',1000)], bar_width, label='default', color=palette[-1])
+ax.bar(bar_width+gap_width, baselinesRequest[('netmarks',1000)], bar_width, label='netMarks', color=palette[-2])
+
+for i, cw in enumerate(cost_weights):
+    sameNetRequests = []
+    for j, nw in enumerate(net_weights):
+        for key, value in requests.items():
+            if key[0] == nw and key[2] == cw:
+                sameNetRequests.append(value)
+    ax.bar(baselines * (bar_width+gap_width) + index + i * bar_width, sameNetRequests, bar_width, label=f'Cost Weight {cw}', color=palette[i])
+
+ax.set_xlabel('Net Weight(Relative)')
+ax.set_ylabel('Total Requests')
+ax.set_title('Total Requests by Net Weight and Cost Weight')
+xticks = [i * (bar_width+gap_width) for i in range(baselines)]
+xticks.extend(baselines * (bar_width+gap_width) + index + group_width / 2 - bar_width / 2)
+print(xticks)
+ax.set_xticks(xticks)
+ax.set_xticklabels(['default', 'netMarks'] + [f'{weight}' for weight in net_weights])
+ax.tick_params(axis='x', labelrotation=45)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(cost_weights), fontsize='x-small')
+
+plt.tight_layout()
+plt.savefig('thesis_plots/images/Total_Requests_1000user.png')
